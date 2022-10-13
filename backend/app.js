0aeb68445bf29d535b075bd5c595133373670e7f
Alexis Janssens <Alexisjanssens@hotmail.fr>
2022-10-13 20:21:15 +0200

    Route auth sans token

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,17 @@ const sauceRoutes = require('./routes/sauce')
 const userRoutes = require('./routes/user')
 
 
-mongoose.connect('mongodb+srv://PiiquantesAdmin:admin@cluster0.g4jrx7t.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://PiiquantesAdmin:admin@piiquantes.2hiub6x.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'))
 ;
 
+
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
