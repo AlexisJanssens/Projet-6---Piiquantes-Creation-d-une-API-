6cbe900656547d5a9dda6cd06c0b0693e0677de6
Alexis Janssens <Alexisjanssens@hotmail.fr>
2022-10-14 04:02:55 +0200

    ajout de 'auth' et route 'getAllSauces'

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -24,12 +25,16 @@ exports.login = (req, res, next) => {
             bcrypt.compare(req.body.password, user.password)
             .then(valid => {
                 if (!valid) {
-                    res.status(401).json({ message : 'Paire indentifiant/mot de passe incorrecte'});
+                    return res.status(401).json({ message : 'Paire indentifiant/mot de passe incorrecte'});
                 } else {
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
-                    })
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'qsdkjahzueyudhfjdhfazeakzepsodkdlfoijezkljfsdpfipaozeakzjfdsifakjksdjk',
+                            { expiresIn: '24h' }
+                        )
+                    });
                 }
             })
             .catch(error => {
